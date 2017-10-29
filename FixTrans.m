dbea4ae4dca251e77761cd86594f40707cac77a8
MAHMOUDSHAHEN\Mahmoud <mahmoud.salah@cis.asu.edu.eg>
2017-10-29 23:44:49 +0200

    Warp.m

--- FixTrans.m
+++ FixTrans.m
@@ -1,6 +1,19 @@
-function [FixedSheet, xTrans, yTrans]= FixTrans(ModelSheet, TransSheet)
+function [FixedSheet, xTrans, yTrans]= FixTrans(m1, a)
 %FIXTRANS Summary of this function goes here
 %   Detailed explanation goes here
+
+% m1 = imread('Model.bmp');
+% a = imread('Ans1.bmp');
+m = rgb2gray(m1);
+
+m = double(m);
+a = double(a);
+
+F = fftshift(fft2(m));
+G = fftshift(fft2(a));
+
+Fimag = conj(F);
+FixedSheet = (G.*Fimag)/abs(G.*Fimag);
 
 
 end
