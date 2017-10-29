aea8a89267893659bdd6eafb2d67a9af3289a32f
MAHMOUDSHAHEN\Mahmoud <mahmoud.salah@cis.asu.edu.eg>
2017-10-30 00:29:52 +0200

    Warp.m

--- Warp.m
+++ Warp.m
@@ -5,7 +5,9 @@ pts1(3,:)=1;
 pts2(3,:)=1;
 
 W = (pts2*pts1')*((pts1*pts1')^-1);
+% P1 = [70 335 280 12; 16 133 280 167; 1 1 1 1];
+% cc = ceil(W*P1;
 
 result = GeometricLinearTransform(I1,W);
-
+% result = res(cc(1,1):cc(1,3), cc(2,1):cc(2,3), :);
 end
